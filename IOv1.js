0693bbd03e81e652bada54179b98b98bdfa5e35f
Thomas <ThomasWT@users.noreply.github.com>
2019-05-22 10:37:15 +0200

    Added support for multiple images.
    Changed object to array to support multiple images entering the viewport at once.

--- IOv1.js
+++ IOv1.js
@@ -4,30 +4,35 @@
  * https://vivekkumar11432.wordpress.com
  * https://github.com/vivek11432
  */
-
 document.addEventListener("DOMContentLoaded", function(event) {
-	function set_image_src(image_tag){
-		image_tag.src = image_tag.dataset.src;
-	};
-	if ('IntersectionObserver' in window) {
-		// Create new IntersectionObserver
-		const io = new IntersectionObserver(function (image_tags) {
-			// Element enters the viewport
-			if(image_tags[0].intersectionRatio !== 0) {
-				set_image_src(image_tags[0].target);
-				// Element leaves the viewport
-			} else {
-				console.log("invisible");
-			}
-		});
-		// Start observing for each image tag
-		document.querySelectorAll('img[data-src]').forEach(function (image_tag) {
-			io.observe(image_tag);
-		});
-	}
-	else {
-		document.querySelectorAll('img[data-src]').forEach(function (image_tag) {
-			image_tag.src = image_tag.dataset.src;
-		});
-	}
+    function set_image_src(image_tag) {
+        image_tag.src = image_tag.dataset.src;
+    };
+
+    function set_iframe_src(iframe_tag) {
+        iframe_tag.src = iframe_tag.dataset.src;
+        resizeIframe(iframe_tag)
+    };
+    if ('IntersectionObserver' in window) {
+        // Create new IntersectionObserver
+        const io = new IntersectionObserver(function(image_tags) {
+            // Element enters the viewport
+            image_tags.forEach(function(entry) {
+                if (entry.intersectionRatio !== 0) {
+                    set_image_src(entry.target);
+                    // Element leaves the viewport
+                }
+            })
+        });
+        // Start observing for each image tag
+        var elements = document.querySelectorAll('img[data-src]')
+        Array.from(elements).forEach(function(image_tag) {
+            io.observe(image_tag);
+        });
+    } else {
+        var elements = document.querySelectorAll('img[data-src]')
+        Array.from(elements).forEach(function(image_tag) {
+            image_tag.src = image_tag.dataset.src;
+        });
+    }
 });
