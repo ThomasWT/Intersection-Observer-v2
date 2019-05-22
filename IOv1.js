2c06fe4dbff3b2dce9f12d2a049a59c84dfe1359
Thomas <ThomasWT@users.noreply.github.com>
2019-05-22 10:49:50 +0200

    Added support for iframes and videos
    Added support for videos

--- IOv1.js
+++ IOv1.js
@@ -1,9 +1,3 @@
-/**
- * Created by Vivek Kumar on 09/09/17.
- * vivekgari@gmail.com
- * https://vivekkumar11432.wordpress.com
- * https://github.com/vivek11432
- */
 document.addEventListener("DOMContentLoaded", function(event) {
     function set_image_src(image_tag) {
         image_tag.src = image_tag.dataset.src;
@@ -29,6 +23,47 @@ document.addEventListener("DOMContentLoaded", function(event) {
         Array.from(elements).forEach(function(image_tag) {
             io.observe(image_tag);
         });
+
+        //lazyload videos
+        var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+        var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(video) {
+                if (video.isIntersecting && $(video.target).is(":visible")) {
+                    for (var source in video.target.children) {
+                        var videoSource = video.target.children[source];
+                        if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                            videoSource.src = videoSource.dataset.src;
+                        }
+                    }
+
+                    video.target.load();
+                    video.target.classList.remove("lazy");
+                    lazyVideoObserver.unobserve(video.target);
+                }
+            });
+        });
+
+        lazyVideos.forEach(function(lazyVideo) {
+            lazyVideoObserver.observe(lazyVideo);
+        });
+
+        //lazyload iframes
+        // Create new IntersectionObserver
+        const iframeO = new IntersectionObserver(function(iframe_tags) {
+            // Element enters the viewport
+            iframe_tags.forEach(function(entry) {
+                if (entry.intersectionRatio !== 0) {
+                    set_iframe_src(entry.target);
+                    // Element leaves the viewport
+                }
+            })
+        });
+        // Start observing for each iframe tag
+        var iframe_elements = document.querySelectorAll('iframe[data-src]')
+        Array.from(iframe_elements).forEach(function(iframe_tag) {
+            iframeO.observe(iframe_tag);
+        });
+
     } else {
         var elements = document.querySelectorAll('img[data-src]')
         Array.from(elements).forEach(function(image_tag) {
